9144133f12d1bb9bde9b1fd3aad5511515184aac
RoLo <61945227+rolodoom@users.noreply.github.com>
2024-08-05 10:37:16 -0500

    Autocomplete and Snippets

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,8 +1,5 @@
 return {
 	"nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvimtools/none-ls-extras.nvim",
-  },
 	config = function()
 		local null_ls = require("null-ls")
 
@@ -10,10 +7,9 @@ return {
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.formatting.black,
-				null_ls.builtins.formatting.isort,
+        -- null_ls.builtins.formatting.black,
+				-- null_ls.builtins.formatting.isort,
 				null_ls.builtins.diagnostics.rubocop,
-        require("none-ls.diagnostics.eslint_d"),
 				null_ls.builtins.formatting.rubocop,
 			},
 		})
